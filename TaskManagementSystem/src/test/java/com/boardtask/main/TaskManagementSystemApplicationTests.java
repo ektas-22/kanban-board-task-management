9c7e0bb32029e334d3769d1e70ecaee45cd7ba31
Ekta S <ektasollet12@gmail.com>
2026-06-09 22:08:41 +0530

    Project package creation

---
+++ TaskManagementSystem/src/test/java/com/boardtask/main/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardtask.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
